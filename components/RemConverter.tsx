c8f7222cbe399f1addf13ce6e357d78b5ea79762
John Correa <jecl2912@gmail.com>
2024-01-07 01:32:51 -0500

    fix: packages

--- components/RemConverter.tsx
+++ components/RemConverter.tsx
@@ -20,7 +20,7 @@ const CssConverter: React.FC = () => {
             return `${remValue}rem`;
         });
 
-        return remCss;
+        return remCss; 
     };
 
     return (
@@ -28,22 +28,24 @@ const CssConverter: React.FC = () => {
             <h1 className="text-4xl font-bold text-blue-500 mb-8">
                 CSS PX to REM Converter
             </h1>
-            <div className="flex flex-col px-8 ">
+            <div className="flex flex-col w-full px-8 ">
                 <div className={"flex md:flex-row w-full gap-6"}>
                     <div className="w-full md:w-1/2 p-6 mb-4 md:mb-0 bg-gray-800 rounded-lg shadow-md overflow-y-auto">
                         <label className="block mb-2 text-lg font-medium">
                             Pega tus estilos CSS aquí:
                         </label>
-                        <code
+                        <textarea
+                            rows={14}
                             className="w-full p-2.5 text-sm bg-gray-700 rounded-lg border border-gray-600 focus:ring-blue-500 focus:border-blue-500 block"
-                            onInput={(e) => setCssInput(e.currentTarget.textContent || '')}
-                        >
-                            {cssInput}
-                        </code>
+                            value={cssInput}
+                            onChange={(e) => setCssInput(e.target.value)}
+                        />
                     </div>
                     <div className="w-full md:w-1/2 p-6 bg-gray-800 rounded-lg shadow-md overflow-y-auto">
-                        <div>
-                            <p className="text-lg font-medium mb-2">Estilos convertidos a rem:</p>
+                    <div>
+                            <p className="text-lg font-medium mb-2">
+                                Estilos convertidos a rem:
+                            </p>
                             <code className="text-sm bg-gray-700 p-4 rounded-lg block">
                                 {convertedCss}
                             </code>
@@ -61,7 +63,6 @@ const CssConverter: React.FC = () => {
             </div>
         </div>
     );
-
 };
 
 export default CssConverter;
