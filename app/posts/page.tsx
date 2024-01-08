5d969fefc07da3391412d0836ffaa6086787d57c
John Correa <jecl2912@gmail.com>
2024-01-08 01:20:21 -0500

    fix: styles post

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,30 +1,46 @@
-import {createClient} from "@/utils/supabase/server";
-import {cookies} from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 import Link from "next/link";
 
-async function getPosts() {
-    const {data, error} = await createClient(cookies()).from("post").select("*")
-    if(error) {
-        throw error
+interface Post {
+    id: number;
+    title: string;
+    created_at: string;
+}
+
+async function getPosts(): Promise<Post[]> {
+    const { data, error } = await createClient(cookies()).from("post").select("*");
+    if (error) {
+        throw error;
     }
 
-    return data
+    return data;
 }
+
 export default async function Posts() {
-    const posts = await getPosts()
-    console.log('Posts', posts)
+    const posts: Post[] = await getPosts();
+
+    console.log('Posts', posts);
+
+    function formatDate(dateString: string): string {
+        const options: Intl.DateTimeFormatOptions = { month: 'short', year: '2-digit' };
+        const [month, year] = new Date(dateString).toLocaleDateString('es-ES', options).split(' ');
+        return `${month.toLowerCase()}. '${year}`;
+    }
+
     return (
         <>
-            <h1>Artículos</h1>
-            <div className="grid grid-cols-2">
-                {
-                    posts?.map((post) => (
-                        <div key={post.id}>
-                            <Link href={`/posts/${encodeURIComponent(`${post.id}`)}`}>{post.title}</Link>
+            <h1 className={"text-center font-bold text-xl lg:text-5xl tracking-wide"}>Blog para la comunidad</h1>
+            <div className="grid divide-y lg:px-8">
+                {posts?.map((post) => (
+                    <Link href={`/posts/${encodeURIComponent(`${post.id}`)}`} key={post.id}>
+                        <div className={"flex justify-between py-6"}>
+                            <span>{post.title}</span>
+                            <span className={"capitalize"}>{formatDate(post.created_at)}</span>
                         </div>
-                        ))
-                }
+                    </Link>
+                ))}
             </div>
         </>
-    )
+    );
 }
