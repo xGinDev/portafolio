17e87c345e8449c4116b22b316ec79f35f8da852
John Correa <jecl2912@gmail.com>
2024-01-08 13:40:56 -0500

    fix: image with dark mode

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,10 +2,23 @@ import logo from '../public/logo-black.svg'
 import logoWhite from '../public/logo-white.svg'
 import Image from "next/image";
 import {useTheme} from "next-themes";
+import {useEffect, useState} from "react";
 
 export default function Logo() {
+  const [mounted, setMounted] = useState(false);
   const { theme} = useTheme();
   console.log('Theme', theme)
+
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <a
       className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover font-bold"
