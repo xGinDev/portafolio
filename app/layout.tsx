9c9b205f7ba41115f887f11d0907c5e1969c341a
John Correa <jecl2912@gmail.com>
2024-01-28 11:12:27 -0500

    feat: metadata in page extension

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from '@vercel/analytics/react';
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+  ? 'https://portafolio-xgindev.vercel.app/'
   : 'http://localhost:3000'
 
 export const metadata = {
@@ -26,17 +26,6 @@ export default function RootLayout({
       <Providers>
           <main>
               <div className="flex flex-row h-screen">
-                  {/*<div className="absolute p-8 lg:relative w-0 -left-[280px] lg:w-[420px] !lg:left-0 lg:left-0 transition-all top-0 h-screen flex flex-col items-center ml-0 bg-secondary">*/}
-                  {/*    <Image*/}
-                  {/*        isZoomed*/}
-                  {/*        width={240}*/}
-                  {/*        alt="NextUI Fruit Image with Zoom"*/}
-                  {/*        src={`${Profile.src}`}*/}
-                  {/*        radius='full'*/}
-                  {/*        className='aspect-square'*/}
-                  {/*    />*/}
-                  {/*    /!*<h1 className={'flex items-center gap-2 text-sm transition-all duration-150 text-foreground-light hover:text-foreground hover:cursor-pointer '}>John Correa (xGinDev)</h1>*!/*/}
-                  {/*</div>*/}
                   <div className="w-full min-h-screen transition-all ease-out overflow-auto lg:ml-0">
                       <Header/>
                       <div className="min-h-screen pt-24 px-8 lg:px-0">
@@ -44,7 +33,6 @@ export default function RootLayout({
                           <SpeedInsights />
                           <Analytics />
                       </div>
-                      {/*<Footer/>*/}
                   </div>
               </div>
           </main>
--- app/vtex-extension/page.tsx
+++ app/vtex-extension/page.tsx
@@ -1,5 +1,10 @@
 import VTEXExtension from "@/components/VtexExtension";
 
+export const metadata = {
+    title: 'VTEX IO Extensión Visual Studio Code',
+    description: '',
+}
+
 export default async function VtexExtension() {
     return (
         <VTEXExtension/>
