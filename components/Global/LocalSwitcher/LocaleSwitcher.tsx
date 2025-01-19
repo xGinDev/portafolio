09d4f92d6a88e9b78abdb7d6f5eba888904f38ea
xGinDev <jecl2912@gmail.com>
2025-01-19 15:35:26 -0500

    fix: all

--- components/Global/LocalSwitcher/LocaleSwitcher.tsx
+++ components/Global/LocalSwitcher/LocaleSwitcher.tsx
@@ -3,11 +3,11 @@ import { routing } from "@/i18n/routing";
 import LocaleSwitcherSelect from "./LocaleSwitcherSelect";
 
 export default function LocaleSwitcher() {
-  const t = useTranslations("LocaleSwitcher");
+  const t = useTranslations("Languajes");
   const locale = useLocale();
 
   return (
-    <LocaleSwitcherSelect defaultValue={locale} label={t("label")}>
+    <LocaleSwitcherSelect defaultValue={locale} label={t("title")}>
       {routing.locales.map((cur) => (
         <option key={cur} value={cur}>
           {t("locale", { locale: cur })}
